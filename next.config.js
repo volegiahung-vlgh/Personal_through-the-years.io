dc369f4a6429e2f446f5ee72486e5290d3132f1a
Vo Le Gia Hung (hung.vo@unibenfoods.com) <hung.vo@unibenfoods.com>
2026-05-29 15:58:32 +0700

    Add GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -6,10 +6,13 @@ const withMDX = require('@next/mdx')({
   },
 });
 
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: isProd ? '/Personal_through-the-years.io' : '',
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   images: {
     unoptimized: true,
